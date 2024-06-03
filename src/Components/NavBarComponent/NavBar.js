a48c766006f49a417e8abd4998279c819ad28d50
sanjanaynvsdl <sanjanayalamarthi@gmail.com>
2024-06-03 12:15:56 +0530

    changed some files

--- src/Components/NavBarComponent/NavBar.js
+++ src/Components/NavBarComponent/NavBar.js
@@ -39,10 +39,9 @@ function NavBar() {
           <Link to='/skills'><FontAwesomeIcon icon={faTools} className="fa-icon" />Skills</Link>
         </li>
         <li className="nav-item">
-          <a href="projects"><FontAwesomeIcon icon={faProjectDiagram} className="fa-icon" />Projects</a>
+          <Link to='/projects'><FontAwesomeIcon icon={faProjectDiagram} className="fa-icon" />Projects</Link>
         </li>
         <li className="nav-item">
-          {/* <a href="link"><FontAwesomeIcon icon={faFile} className="fa-icon" />Resume</a> */}
            <a href="https://drive.google.com/file/d/13kaPsdMNDsM4LV9g7m5-E5PTildp-yYf/view" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFile} className="fa-icon" />Resume</a>
         </li>
       </ul>
